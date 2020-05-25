3c438a9cdf61cedaae4641f74f9128d9de52ff4e
Vijay <ssvijayg@gmail.com>
2020-05-25 21:32:02 +0530

    DL INIT

--- jarvis.py
+++ jarvis.py
@@ -6,6 +6,27 @@ import webbrowser
 import os
 import smtplib
 
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+
+import json
+with open('intents.json') as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+
+#20
+
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
